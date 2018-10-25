30416ef1a16c930787f487797efba673d200d596
zhuozhongmeng <164538190@qq.com>
2018-10-26 01:32:15 +0800

    增加了X的偏置，对b的求导记得是复合函数求导规则来求

--- 1D/2D.py
+++ 1D/2D.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 
 data = np.loadtxt("data2d.txt")
 print(data)
-times = 1000
-rate = 0.0001
+times = 3000
+rate = 0.00001
 x=[]
 y=[]
 w0 = 0
 w1 = 0
 w2 = 0
+b = -2
 for i in range(len(data)):
     if i < len(data)/2:
         x.append(data[i])
@@ -17,30 +18,44 @@ for i in range(len(data)):
         y.append(data[i])
 
 plt.plot(x,y)
-plt.pause(2)
+plt.pause(0.2)
 plt.close()
 plt.show()
 delta_w0_sum =[]
 delta_w1_sum =[]
 delta_w2_sum =[]
+delta_b_sum=[]
 
 for time in range(times):
     delta_w2 = 0
     delta_w1 = 0
     delta_w0 = 0
+    delta_b = 0
     y_=[]
     for i in range(len(x)):
-        delta_w0 += w2*x[i]**2 +w1*x[i]+w0 - y[i]
-        delta_w1 += (w2*x[i]**2 +w1*x[i]+w0- y[i])*x[i]
-        delta_w2 += (w2*x[i]**2 +w1*x[i]+w0- y[i])*x[i]**2
-        y_.append(w2*x[i]**2+w1*x[i]+w0)
+        delta_w0 += w2*(x[i]+b)**2 +w1*(x[i]+b)+w0 - y[i]
+        delta_w1 += (w2*(x[i]+b)**2 +w1*(x[i]+b)+w0- y[i])*(x[i]+b)
+        delta_w2 += (w2*(x[i]+b)**2 +w1*(x[i]+b)+w0- y[i])*(x[i]+b)**2
+        delta_b += (w2*(x[i]+b)**2 +w1*(x[i]+b)+w0- y[i])*(2*w2*(x[i]+b)+w1)
+        y_.append(w2*(x[i]+b)**2+w1*(x[i]+b)+w0)
     w2 -= rate*delta_w2/len(x)
     w1 -= rate*delta_w1/len(x)
     w0 -= rate*delta_w0/len(x)
-
-    plt.plot(range(len(x)),y_)
-    plt.plot(x,y,'x')
-    plt.pause(0.001)
-    plt.close()
-    plt.show()
-    y_ = []
+    b -= rate*delta_b/len(x)
+    delta_b_sum.append(delta_b)
+    delta_w0_sum.append(delta_w0)
+    delta_w1_sum.append(delta_w1)
+    delta_w2_sum.append(delta_w2)
+    if time %10 == 0:
+        plt.plot(x,y_)
+        plt.plot(x,y,'x')
+        print(b)
+        plt.pause(0.001)
+        plt.close()
+        plt.show()
+        y_ = []
+plt.plot(range(times), delta_w2_sum)
+plt.plot(range(times), delta_w1_sum)
+plt.plot(range(times), delta_w0_sum)
+plt.plot(range(times), delta_b_sum ,"y")
+plt.show()
